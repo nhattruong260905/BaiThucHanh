97b339cb924a6d09abb32c3c72890292074e9e64
nhattruong260905 <151375200+nhattruong260905@users.noreply.github.com>
2023-11-18 19:51:14 +0700

    Add files via upload

---
+++ TH01/BT01/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+/*a*/
+//	printf("Chao cac ban");
+/*b*/
+//printf("Chao\tcac\tban");
+/*c*/
+printf("Chao\ncac\nban");
+	return 0;
+}
